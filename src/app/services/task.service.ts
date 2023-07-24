51f4177b48d288cd6cc323a842e1a1c4086c9a0c
gfizzle <winstongdouglas4@gmail.com>
2023-07-24 01:36:21 -0500

    Adding RxJs observables with HttpClient

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
-import { TASKS } from 'src/app/mock-tasks';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Observable} from 'rxjs'
 import { Task } from 'src/app/Task';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks'
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
-  getTasks(): Task[] {
-    return TASKS;
+  getTasks(): Observable<Task[]> {
+   return this.http.get<Task[]>(this.apiUrl);
   }
 }
